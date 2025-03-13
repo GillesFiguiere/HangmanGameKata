5ec5e3d1589589bcbab3b9c6c31db4219f153524
Deborah DOUILLET <deborah.douillet@adenes.eu>
2025-03-13 11:47:42 +0100

    refactor test

--- Test/HangmanGameKataShould.cs
+++ Test/HangmanGameKataShould.cs
@@ -23,33 +23,22 @@ public class HangmanGameKataShould
         result.Should().Be("####");
     }
 
-    [Fact]
+    [Theory]
+    [InlineData("AZER", 'A', "A###")]
+    [InlineData("AZER", 'Z', "#Z##")]
+    [InlineData("AZER", 'E', "##E#")]
+    [InlineData("AZERA", 'A', "A###A")]
     [Description("Return A### when user types A and word to guess is AZER")]
-    void ReturnWhenUserTypesA()
+    void ReturnWhenUserTypesA(string wordToGuess, char userLetter, string expectedResult)
     {
         // GIVEN
-        var game = new Game("AZER");
+        var game = new Game(wordToGuess);
 
         // WHEN
-        var result = game.Try('A');
+        var result = game.Try(userLetter);
 
         // THEN
-        result.Should().Be("A###");
+        result.Should().Be(expectedResult);
     }
-    
-    [Fact]
-    [Description("Return #Z## when user types Z and word to guess is AZER")]
-    void ReturnWhenUserTypesZ()
-    {
-        // GIVEN
-        var game = new Game("AZER");
-
-        // WHEN
-        var result = game.Try('Z');
-
-        // THEN
-        result.Should().Be("#Z##");
-    }
-    
     
 }
