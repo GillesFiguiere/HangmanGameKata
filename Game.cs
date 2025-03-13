c52c15400059c03c9e5106007e84b2e1cc3d49ec
Gilles Figuière <me@gillesfiguiere.com>
2025-03-13 10:53:01 +0100

    Premier test green

---
+++ Game.cs
@@ -0,0 +1,16 @@
+﻿namespace HangmanGameKata;
+
+internal class Game
+{
+    private readonly string _wordToGuess;
+
+    public Game(string wordToGuess)
+    {
+        _wordToGuess = wordToGuess;
+    }
+
+    public string Try(char userLetter)
+    {
+        return "####";
+    }
+}
---
+++ Test/HangmanGameKataShould.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using FluentAssertions;
+using Xunit;
+
+namespace HangmanGameKata.Test;
+
+/// <summary>
+/// - 
+/// </summary>
+public class HangmanGameKataShould
+{
+    [Fact]
+    [Description("Return #### when user types Q and word to guess is AZER")]
+    void ReturnWhenUserTypesQ()
+    {
+        // GIVEN
+        var game = new Game("AZER");
+
+        // WHEN
+        var result = game.Try('Q');
+
+        // THEN
+        result.Should().Be("####");
+    }
+}
