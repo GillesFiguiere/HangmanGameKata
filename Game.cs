8550b4425fc72d621647816345086b6857180529
Gilles Figuière <me@gillesfiguiere.com>
2025-03-14 12:33:25 +0100

    2 tries test green

--- Game.cs
+++ Game.cs
@@ -5,20 +5,29 @@ namespace HangmanGameKata;
 internal class Game
 {
     private readonly string _wordToGuess;
+    private string _hidenWordToGuess;
 
     public Game(string wordToGuess)
     {
         _wordToGuess = wordToGuess.ToUpper();
+        _hidenWordToGuess = string.Concat(Enumerable.Repeat('#', _wordToGuess.Length));
     }
 
     public string Try(char userLetter)
     {
         char userLetterUpper = char.ToUpper(userLetter);
-        
-        var result = _wordToGuess.Aggregate("", (wipResult, wordToGuessCurrentLetter) => 
-            wordToGuessCurrentLetter == userLetterUpper ? wipResult + userLetterUpper : wipResult + "#"
-        );
 
-        return result;
+        var result = "";
+        for (var index = 0; index < _wordToGuess.Length; index++)
+        {
+            var c = _wordToGuess[index];
+            result = c == userLetterUpper
+                ? result + userLetterUpper
+                : result + _hidenWordToGuess[index];
+        }
+
+        _hidenWordToGuess = result;
+
+        return _hidenWordToGuess;
     }
 }
