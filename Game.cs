7058b7e28ce84955433140fc68fd4bc45e324c46
Anthony CHRETIEN <anthony.chretien@adenes.eu>
2025-04-03 10:48:14 +0200

    Mise en place test green où il y a plusieurs appels de la méthode Try

--- Game.cs
+++ Game.cs
@@ -5,19 +5,34 @@ namespace HangmanGameKata;
 internal class Game
 {
     private readonly string _wordToGuess;
+    private string _currentGuess;
 
     public Game(string wordToGuess)
     {
         _wordToGuess = wordToGuess.ToUpper();
+        _currentGuess = string.Concat(Enumerable.Repeat('#', wordToGuess.Length));
     }
 
     public string Try(char userLetter)
     {
+
         char userLetterUpper = char.ToUpper(userLetter);
-        
-        var result = _wordToGuess.Aggregate("", (wipResult, wordToGuessCurrentLetter) => 
-            wordToGuessCurrentLetter == userLetterUpper ? wipResult + userLetterUpper : wipResult + "#"
-        );
+      
+        var result = string.Empty;
+
+        for(int i = 0; i < _wordToGuess.Length;i++)
+        {
+            char c = _wordToGuess[i];
+            if (_currentGuess[i] != '#' )
+            {
+                result += _currentGuess[i];
+            }
+            else
+            {
+                result += userLetterUpper == c ?  userLetterUpper : "#";
+            }
+        }
+        _currentGuess = result;
 
         return result;
     }
